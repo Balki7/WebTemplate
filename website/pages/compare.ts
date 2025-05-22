d4a1952a6110f5f7914446073cb7887ba548f41c
Balki7 <s333562932@ariel.ort.org.il>
2025-05-22 08:53:55 +0300

    הוספתי עוד שתי עמודות

--- website/pages/compare.ts
+++ website/pages/compare.ts
@@ -1,12 +1,7 @@
 import { send } from "../utilities";
 
-console.log("hello");
-
 let logOutButton = document.getElementById("logOutButton") as HTMLButtonElement;
-
 logOutButton.onclick = async function (): Promise<void> {
-    // await send("logOut", []);
-    console.log("hello");
     localStorage.removeItem("userId");
     location.href = "index.html";
 };
